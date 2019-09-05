2ff0bb85b7f046921bff7133f030c10211eafab3
stevenluong72 <luongminhduc94@gmail.com>
2019-09-05 14:55:54 +0700

    checkbox + modal delete confirm

--- public/ajax.js
+++ public/ajax.js
@@ -1,39 +1,38 @@
 $(function() {
 	const API_ROOT = '/films';
-
 	const fetchData = () => {
 		$.ajax({
 			url: API_ROOT,
 			type: 'GET',
 			dataType: 'json',
 			beforeSend: function() {
-				$('#no_result').text('Loading...');
+				$('#alert').text('Loading...');
 			},
 			complete: function() { //giống kiểu .always()
-				$('#no_result').text('Done!').fadeOut(2000);
+				$('#alert').text('');
 			},
 			error: function() { //giống kiểu .fail()
-				$('#no_result').text('Failed');
+				$('#alert').text('Failed');
 			},
 			success: function(films) { //giống kiểu .done()
 				let results = [];
-				let compare = {
-					name: function(a,b) {
-						if (a < b) return -1;
-						else if (a > b) return 1;
-						else return 0;
-					},
-					duration: function(a,b) {
-						a = a.replace(":", "");
-						b = b.replace(":", "");
-						return a - b;
-					},
-					date: function(a,b) {
-						a = new Date(a);
-						b = new Date(b);
-						return a - b;
-					}
-				}
+				// let compare = {
+				// 	name: function(a,b) {
+				// 		if (a < b) return -1;
+				// 		else if (a > b) return 1;
+				// 		else return 0;
+				// 	},
+				// 	duration: function(a,b) {
+				// 		a = a.replace(":", "");
+				// 		b = b.replace(":", "");
+				// 		return a - b;
+				// 	},
+				// 	date: function(a,b) {
+				// 		a = new Date(a);
+				// 		b = new Date(b);
+				// 		return a - b;
+				// 	}
+				// }
 
 				function renderContent(array) {
 					// xoá hết cái ban đầu đi render lại
@@ -41,60 +40,74 @@ $(function() {
 					//lặp object films để đổ dữ liệu
 					array.forEach( function(element) {
 						$('tbody').append(`
-							<tr>
+							<tr data-id="${element.id}">
+								<td><input class="checkbox" type="checkbox" /></td>
 								<td>${element.genre}</td>
 								<td>${element.title}</td>
 								<td>${element.duration}</td>
 								<td>${element.date}</td>
 								<td>
-									<a data-id="${element.id}" class="edit" href="javascript:void(0)">Edit</a> | 
-									<a data-id="${element.id}" class="delete" href="javascript:void(0)">Delete</a></td>
+									<i data-id="${element.id}" class="edit fas fa-edit">&nbsp;Edit
 							</tr>
 						`);
 					});
 
 					bindEvent();
+			
+					if (array.length === 0) $('#no_result').text(`No Entries`).show();
+					else $('#no_result').text(``);
 				}
 				renderContent(films);
 
 				function bindEvent() {
-					$('.delete').on('click', function(event) {
-						event.preventDefault();
-
-						if (confirm("Are you sure you want to delete it?")) {						
-							$(this).parent().parent().hide(400);
-
-							$.ajax({
-								url: API_ROOT + `/${$(this).data('id')}`,
-								type: 'DELETE'
-							})
-							.done(function() {
-								console.log("success");
-							})
-							.fail(function() {
-								console.log("error");
-							})
-							.always(function() {
-								console.log("complete");
-							});
-						} else return;
-
-					});
-
 					// When "Edit" is clicked
 					$('.edit').on('click', function(event) {
 						event.preventDefault();
 						let td_Siblings = $(this).parent().siblings();
 
 						putModal.find('legend').attr('data-id', $(this).data('id'));
-						putModal.find('#genre').val(td_Siblings[0].textContent);
-						putModal.find('#title').val(td_Siblings[1].textContent);
-						putModal.find('#duration').val(td_Siblings[2].textContent);
-						putModal.find('#date').val(td_Siblings[3].textContent);
+						putModal.find('#genre').val(td_Siblings[1].textContent);
+						putModal.find('#title').val(td_Siblings[2].textContent);
+						putModal.find('#duration').val(td_Siblings[3].textContent);
+						putModal.find('#date').val(td_Siblings[4].textContent);
 
 						putModal.show();
 						putModal.find('#genre').select();
 						
+					});
+
+					// Check-all button
+					$('th input[type="checkbox"]').on('change', function() {
+						$('.checkbox').prop('checked', $(this).prop('checked'));
+
+						if ($(this).prop('checked') === true) {
+							if ($('.checkbox')[0]) $('#delete-selected').fadeIn(200);
+						} else $('#delete-selected').fadeOut(200);
+
+					});
+
+					// Clear check-all button if one is deselected
+					$('.checkbox').on('change', function(event) {
+						if (!event.target.checked) $('th input[type="checkbox"]').prop('checked', false);
+
+						if (!$('.checkbox:checked')[0]) $('#delete-selected').fadeOut(200);
+						else $('#delete-selected').fadeIn(200);
+					});
+
+					$('#delete-selected').on('click', function() {
+						let ids = [];
+						let trs_checked = $('.checkbox:checked').parent().parent();
+
+						// Get ids
+						trs_checked.each(function(index, el) {
+							ids.push($(el).attr('data-id'));
+						});
+						
+						console.log(ids)
+
+						$('#notiModal').fadeIn(200);
+						$('#notiModal').find('.yes').attr('data-id', ids);
+		
 					});
 				}
 
@@ -150,15 +163,12 @@ $(function() {
 								}
 							}
 						});
-						
-						// Thông báo No results
-						if (!results[0]) $('#no_result').text(`No results`).show();
-						else $('#no_result').text(``);
+				
 
 						renderContent(results);
 						
 						//Highlight search results
-						$('tr').find('td:not(td:last-child)').each(function(index, el) {
+						$('tr').find('td:not(td:first-child, td:last-child)').each(function(index, el) {
 							$(this).html($(this).html().toLowerCase().replace(re, `<mark>$&</mark>`));
 						});
 					}
@@ -188,6 +198,7 @@ $(function() {
 		})
 		.done(function() {
 			console.log("success");
+			// Re-fetch data
 			fetchData();
 		})
 		.fail(function() {
@@ -227,9 +238,43 @@ $(function() {
 		
 	});
 
+	$('.yes').on('click', function() {
+		let ids = $(this).attr('data-id').split(',');
+		console.log(ids)
+
+
+		// Send multiple DELETE requests
+		ids.forEach( function(element, index) {
+
+			$.ajax({
+				url: API_ROOT + '/' + element,
+				type: 'DELETE'
+			})
+			.done(function() {
+				console.log("success");
+				// Hide modal
+				$('#notiModal').fadeOut(300);
+
+				// Hide rows
+				$('.checkbox:checked').parent().parent().remove();
+
+				// Hide delete-selected button if there's nothing left
+				if (!$('.checkbox')[0]) $('#delete-selected').hide();
+			})
+			.fail(function() {
+				console.log("error");
+				alert("Delete failed!");
+			})
+			.always(function() {
+				console.log("complete");
+			});	
+		});			
+	});
+
 
 	let postModal = $('#postModal');
 	let putModal = $('#putModal');
+	let notiModal = $('#notiModal');
 
 	$('h3').on('click', function() {
 		postModal.show();
@@ -238,13 +283,19 @@ $(function() {
 	});
 
 	$('.close').on('click', function() {
-		postModal.hide();
-		putModal.hide();
+		postModal.fadeOut(300);
+		putModal.fadeOut(300);
+		notiModal.fadeOut(300);
+	});
+
+	$('.no').on('click', function() {
+		notiModal.fadeOut(300);
 	});
 
 	$(window).on('click', function(event) {
-		if (event.target == $('#postModal')[0]) postModal.hide();
-		if (event.target == $('#putModal')[0]) putModal.hide();
+		if (event.target == $('#postModal')[0]) postModal.fadeOut(300);
+		if (event.target == $('#putModal')[0]) putModal.fadeOut(300);
+		if (event.target == $('#notiModal')[0]) notiModal.fadeOut(300);
 	});
 
 });
